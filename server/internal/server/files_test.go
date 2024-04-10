43d1013c8fbff808927148e2098707f4a8d2f102
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-04-10 16:23:49 -0700

    Add a test for CreateFile (#11)

--- server/internal/server/files_test.go
+++ server/internal/server/files_test.go
@@ -1,7 +1,12 @@
 package server
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"mime/multipart"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	v1 "github.com/llm-operator/file-manager/api/v1"
@@ -55,4 +60,51 @@ func TestFiles(t *testing.T) {
 	listResp, err = srv.ListFiles(ctx, &v1.ListFilesRequest{})
 	assert.NoError(t, err)
 	assert.Len(t, listResp.Data, 0)
+}
+
+func TestCreateFile(t *testing.T) {
+	st, tearDown := store.NewTest(t)
+	defer tearDown()
+
+	srv := New(st)
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		srv.CreateFile(w, r, nil)
+	})
+
+	var b bytes.Buffer
+	w := multipart.NewWriter(&b)
+
+	fw, err := w.CreateFormFile("file", "test-file.jsonl")
+	assert.NoError(t, err)
+	_, err = fw.Write([]byte("hello"))
+	assert.NoError(t, err)
+
+	fw, err = w.CreateFormField("purpose")
+	assert.NoError(t, err)
+	_, err = fw.Write([]byte(purposeFineTune))
+	assert.NoError(t, err)
+
+	w.Close()
+
+	req, err := http.NewRequest("POST", "v1/files", &b)
+	assert.NoError(t, err)
+	req.Header.Set("Content-Type", w.FormDataContentType())
+
+	handler.ServeHTTP(rr, req)
+	assert.Equal(t, http.StatusCreated, rr.Code)
+
+	var fj fileJSON
+	err = json.Unmarshal(rr.Body.Bytes(), &fj)
+	assert.NoError(t, err)
+	assert.True(t, fj.ID != "")
+	assert.Equal(t, purposeFineTune, fj.Purpose)
+	assert.Equal(t, "test-file.jsonl", fj.Filename)
+	assert.Equal(t, int64(5), fj.Bytes)
+
+	resp, err := srv.GetFile(context.Background(), &v1.GetFileRequest{
+		Id: fj.ID,
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, fj.ID, resp.Id)
 }
