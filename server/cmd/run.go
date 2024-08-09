ac8029cb072c373c50a36e82a16071f6c9db8266
Aya (Igarashi) Ozawa <Ladiclexxx@gmail.com>
2024-08-09 03:41:57 +0200

    Configure securityContext, resources, and probes in the deployments (#76)

--- server/cmd/run.go
+++ server/cmd/run.go
@@ -18,6 +18,7 @@ import (
 	"github.com/spf13/cobra"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/protobuf/encoding/protojson"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -67,6 +68,12 @@ func run(ctx context.Context, c *config.Config) error {
 		return err
 	}
 
+	addr := fmt.Sprintf("localhost:%d", c.GRPCPort)
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	conn, err := grpc.NewClient(addr, opts...)
+	if err != nil {
+		return err
+	}
 	mux := runtime.NewServeMux(
 		runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
 			// Do not use the camel case for JSON fields to follow OpenAI API.
@@ -79,9 +86,8 @@ func run(ctx context.Context, c *config.Config) error {
 			},
 		}),
 		runtime.WithIncomingHeaderMatcher(auth.HeaderMatcher),
+		runtime.WithHealthzEndpoint(grpc_health_v1.NewHealthClient(conn)),
 	)
-	addr := fmt.Sprintf("localhost:%d", c.GRPCPort)
-	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
 	if err := v1.RegisterFilesServiceHandlerFromEndpoint(ctx, mux, addr, opts); err != nil {
 		return err
 	}
